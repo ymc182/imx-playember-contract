e8c0932aa1c5997311dd44b690245ef3b7f0d7e3
ymc182 <ymc182@gmail.com>
2024-07-16 20:21:21 +1000

    subgraph

---
+++ script/PackPayment/setPrice.ts
@@ -0,0 +1,19 @@
+import hre, { ethers } from "hardhat";
+import { PackPayment } from "../../typechain-types";
+async function main() {
+	const [deployer] = await hre.ethers.getSigners();
+	const PackPayment = await hre.ethers.getContractFactory("PackPayment");
+	const contractAddress = "0xDd66e40689eAbcA00C70EC25f1857516cf312a03";
+	const packPayment = PackPayment.attach(contractAddress) as PackPayment;
+	await packPayment.setNativePrice(1, ethers.parseUnits("0.001", 18));
+
+	const packs = await packPayment.getAllPacks();
+	console.log(packs);
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
---
+++ script/PackPayment/testBuy.ts
@@ -0,0 +1,21 @@
+import hre, { ethers } from "hardhat";
+import { PackPayment } from "../../typechain-types";
+async function main() {
+	const [deployer] = await hre.ethers.getSigners();
+	const PackPayment = await hre.ethers.getContractFactory("PackPayment");
+	const contractAddress = "0xDd66e40689eAbcA00C70EC25f1857516cf312a03";
+	const packPayment = PackPayment.attach(contractAddress) as PackPayment;
+	await packPayment.buyPackWithNative(1, "ymc@ewtd.io", {
+		value: ethers.parseEther("0.001"),
+	});
+
+	const packs = await packPayment.getAllPacks();
+	console.log(packs);
+}
+
+main()
+	.then(() => process.exit(0))
+	.catch((error) => {
+		console.error(error);
+		process.exit(1);
+	});
